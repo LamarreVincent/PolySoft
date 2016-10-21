5eb659e4204f8c807222895a9f24adab6fca2e1a
Ariane Tourangeau <ariane.tourangeau@polymtl.ca>
2016-10-21 09:25:40 -0400

    Include angular module. The project is ready :)

---
+++ HackHarvard2016/HackHarvard2016/www/scripts/module.js
@@ -0,0 +1,7 @@
+﻿(function() {
+
+    var harvard = angular.module("HackHarvard", []);
+    harvard.controller("indexController", ['$scope', function ($scope) {
+        $scope.name = "WE ARE THE CHAMPION";
+    }]);
+})()
